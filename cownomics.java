4b1ae3b4cee1129783fc53f07da68f752f2dcb49
betaCarrot <30773614+betaCarrot@users.noreply.github.com>
2017-11-21 23:51:21 +0800

    USACO Open 2017 Cu #2

--- cownomics.java
+++ cownomics.java
@@ -1,63 +1,76 @@
-import java.util.*;
-import java.io.*;
-import java.lang.Math;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
-public class cownomics{
-	public static void main(String[] args) throws IOException{
+public class cownomics {
+	private static final int A = 0;
+	private static final int C = 1;
+	private static final int G = 2;
+	private static final int T = 3;
+	private static int[][] spots;
+	private static int[][] plains;
+	private static int N, M;
+	private static int result = 0;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("cownomics.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("cownomics.out")));
 		StringTokenizer st = new StringTokenizer(f.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int width = Integer.parseInt(st.nextToken());
-		int[][] spotted = new int[N][width];
-		int[][] plain = new int[N][width];
-		for(int i=0;i<N;i++){
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		spots = new int[N][M];
+		plains = new int[N][M];
+		for (int i = 0; i < N; i++) {
 			String line = f.readLine();
-			for(int k=0;k<width;k++){
-				String temp = line.substring(k,k+1);
-				int number = -1;
-				if(temp.equals("A")) number = 0;
-				else if(temp.equals("C")) number = 1;
-				else if(temp.equals("G")) number = 2;
-				else if(temp.equals("T")) number = 3;
-				spotted[i][k] = number;			
+			for (int k = 0; k < M; k++) {
+				String s = line.substring(k, k + 1);
+				if (s.equals("A"))
+					spots[i][k] = A;
+				else if (s.equals("C"))
+					spots[i][k] = C;
+				else if (s.equals("G"))
+					spots[i][k] = G;
+				else
+					spots[i][k] = T;
 			}
 		}
-		for(int i=0;i<N;i++){
+		for (int i = 0; i < N; i++) {
 			String line = f.readLine();
-			for(int k=0;k<width;k++){
-				String temp = line.substring(k,k+1);
-				int number = -1;
-				if(temp.equals("A")) number = 0;
-				else if(temp.equals("C")) number = 1;
-				else if(temp.equals("G")) number = 2;
-				else if(temp.equals("T")) number = 3;
-				plain[i][k] = number;			
+			for (int k = 0; k < M; k++) {
+				String s = line.substring(k, k + 1);
+				if (s.equals("A"))
+					plains[i][k] = A;
+				else if (s.equals("C"))
+					plains[i][k] = C;
+				else if (s.equals("G"))
+					plains[i][k] = G;
+				else
+					plains[i][k] = T;
 			}
 		}
-		int result = 0;
-		for(int a=0;a<width;a++){
-			for(int b=a+1;b<width;b++){
-				for(int c=b+1;c<width;c++){
-					boolean OK = true;
-					boolean[] spottedSequences = new boolean[64];
-					boolean[] plainSequences = new boolean[64];
-					for(int k=0;k<N;k++){
-						int index1 = spotted[k][a]*16+spotted[k][b]*4+spotted[k][c];
-						spottedSequences[index1] = true;
-						int index2 = plain[k][a]*16+plain[k][b]*4+plain[k][c];
-						plainSequences[index2] = true;
-					} 
-					for(int i=0;i<64;i++){
-						if(spottedSequences[i]&&plainSequences[i]) OK = false;
-					}
-					if(OK) {
-						result++;
-					}
-				}
+		for (int k = 0; k < M; k++) {
+			boolean[] gSpots = new boolean[4];
+			boolean[] gPlains = new boolean[4];
+			for (int i = 0; i < N; i++) {
+				gSpots[spots[i][k]] = true;
+				gPlains[plains[i][k]] = true;
 			}
+			if (check(gSpots, gPlains))
+				result++;
 		}
 		out.println(result);
 		out.close();
+	}
+
+	public static boolean check(boolean[] as, boolean[] bs) {
+		for (int i = 0; i <= 3; i++) {
+			if (as[i] == bs[i] && as[i] == true)
+				return false;
+		}
+		return true;
 	}
 }
