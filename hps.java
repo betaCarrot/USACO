fed2434194766ed3cecf39e5efe55a56c74651d5
betaCarrot <30773614+betaCarrot@users.noreply.github.com>
2017-11-21 08:54:04 +0800

    USACO January 2017 Ag #2

--- hps.java
+++ hps.java
@@ -1,38 +1,72 @@
-import java.util.*;
-import java.io.*;
-import java.lang.Math;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class hps{
-	public static void main(String[] args) throws IOException{
+public class hps {
+	private static final int H = 1;
+	private static final int S = 2;
+	private static final int P = 3;
+	private static int N;
+	private static int[] values;
+	private static int[] dpH;
+	private static int[] dpS;
+	private static int[] dpP;
+	private static int result = 0;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("hps.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("hps.out")));
-		int N = Integer.parseInt(f.readLine());
-		int result1 = 0;
-		int result2 = 0;
-		for(int i=0;i<N;i++){
-			StringTokenizer st = new StringTokenizer(f.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			result1+=judge1(a,b);
-			result2+=judge2(a,b);
+		N = Integer.parseInt(f.readLine());
+		values = new int[N];
+		for (int i = 0; i < N; i++) {
+			String line = f.readLine();
+			if (line.equals("H"))
+				values[i] = H;
+			else if (line.equals("S"))
+				values[i] = S;
+			else
+				values[i] = P;
 		}
-		out.println(Math.max(result1,result2));
+		dpH = new int[N];
+		dpS = new int[N];
+		dpP = new int[N];
+		if (values[0] == H)
+			dpH[0] = 1;
+		else if (values[0] == S)
+			dpS[0] = 1;
+		else
+			dpP[0] = 1;
+		for (int i = 1; i < N; i++) {
+			dpH[i] = dpH[i - 1];
+			dpS[i] = dpS[i - 1];
+			dpP[i] = dpP[i - 1];
+			if (values[i] == H)
+				dpH[i]++;
+			else if (values[i] == S)
+				dpS[i]++;
+			else
+				dpP[i]++;
+		}
+		result = Math.max(result, max(dpH[N - 1], dpS[N - 1], dpP[N - 1]));
+		for (int i = 0; i < N; i++) {
+			int before = max(dpH[i], dpS[i], dpP[i]);
+			int after = max(dpH[N - 1] - dpH[i], dpS[N - 1] - dpS[i], dpP[N - 1] - dpP[i]);
+			result = Math.max(result, before + after);
+		}
+		out.println(result);
 		out.close();
 	}
 
-	public static int judge1(int a, int b){
-		if(a==b) return 0;
-		if(a==1&&b==3) return 1;
-		if(a==3&&b==2) return 1;
-		if(a==2&&b==1) return 1;
-		return 0;
+	public static int max(int a, int b, int c) {
+		return Math.max(a, Math.max(b, c));
 	}
 
-	public static int judge2(int a, int b){
-		if(a==b) return 0;
-		if(a==1&&b==2) return 1;
-		if(a==2&&b==3) return 1;
-		if(a==3&&b==1) return 1;
-		return 0;
+	public static void printArray(int[] array) {
+		for (int i : array)
+			System.out.print(i + " ");
+		System.out.println();
 	}
 }
