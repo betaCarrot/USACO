705e7896c6c021a2e8561ea2fc66a0708179600f
betaCarrot <30773614+betaCarrot@users.noreply.github.com>
2017-11-21 23:03:08 -0500

    USACO December 2016 Cu #3

--- cowsignal.java
+++ cowsignal.java
@@ -1,36 +1,40 @@
-import java.util.*;
-import java.io.*;
-import java.lang.Math;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
-public class cowsignal{
-	public static void main(String[] args) throws IOException{
+public class cowsignal {
+	private static int M, N, K;
+	private static String[][] result;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("cowsignal.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("cowsignal.out")));
 		StringTokenizer st = new StringTokenizer(f.readLine());
-		int depth = Integer.parseInt(st.nextToken());
-		int width = Integer.parseInt(st.nextToken());
-		int times = Integer.parseInt(st.nextToken());
-		String[][] original = new String[depth+1][width+1];
-		for(int i=1;i<=depth;i++){
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		result = new String[K * M][K * N];
+		for (int i = 0; i < M; i++) {
 			String line = f.readLine();
-			for(int k=0;k<width;k++){
-				original[i][k+1] = line.substring(k,k+1);
-			}
-		}
-		String[][] result = new String[depth+1][width*times+1];
-		for(int row=1;row<=depth;row++){
-			int index = 1;
-			while(index<=width){
-				for(int i=((index-1)*times+1);i<=index*times;i++) result[row][i] = original[row][index];
-				index++;
+			for (int k = 0; k < N; k++) {
+				String s = line.substring(k, k + 1);
+				for (int row = K * i; row < K * (i + 1); row++) {
+					for (int col = K * k; col < K * (k + 1); col++) {
+						result[row][col] = s;
+					}
+				}
 			}
 		}
 		StringBuilder sb = new StringBuilder();
-		for(int row=1;row<=depth;row++){
-			for(int i=0;i<times;i++){
-				for(int k=1;k<=width*times;k++) sb.append(result[row][k]);
-				sb.append("\n");
+		for (int i = 0; i < result.length; i++) {
+			for (int k = 0; k < result[0].length; k++) {
+				sb.append(result[i][k]);
 			}
+			sb.append("\n");
 		}
 		out.print(sb.toString());
 		out.close();
