e434a2a4916430db9bb87479eac132f087ff2d8a
betaCarrot <30773614+betaCarrot@users.noreply.github.com>
2017-11-23 01:55:16 +0800

    USACO Open 2016 Cu #3

--- reduce.java
+++ reduce.java
@@ -12,12 +12,6 @@ public class reduce {
 	private static int[] ys;
 	private static int[][] cows;
 	private static int N;
-	private static int[][] map = new int[4][4];
-	private static final int LEFT = 0;
-	private static final int RIGHT = 1;
-	private static final int TOP = 2;
-	private static final int BOTTOM = 3;
-	private static int result = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("reduce.in"));
@@ -29,61 +23,44 @@ public class reduce {
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(f.readLine());
 			int x = Integer.parseInt(st.nextToken());
-			xs[i] = x;
 			int y = Integer.parseInt(st.nextToken());
+			xs[i] = x;
 			ys[i] = y;
 			cows[i][0] = x;
 			cows[i][1] = y;
 		}
+		int result = Integer.MAX_VALUE;
 		Arrays.sort(xs);
 		Arrays.sort(ys);
-		for (int i = 0; i < 4; i++) {
-			map[LEFT][i] = xs[i];
+		for (int a = 0; a <= 1; a++) {
+			int minX = xs[a];
+			for (int b = 0; b <= 1; b++) {
+				int minY = ys[b];
+				for (int c = 0; c <= 1; c++) {
+					int maxX = xs[N - 1 - c];
+					for (int d = 0; d <= 1; d++) {
+						int maxY = ys[N - 1 - d];
+						if (valid(minX, minY, maxX, maxY)) {
+							result = Math.min(result, area(minX, minY, maxX, maxY));
+						}
+					}
+				}
+			}
 		}
-		for (int i = 0; i < 4; i++) {
-			map[RIGHT][i] = xs[N - 1 - i];
-		}
-		for (int i = 0; i < 4; i++) {
-			map[TOP][i] = ys[i];
-		}
-		for (int i = 0; i < 4; i++) {
-			map[BOTTOM][i] = ys[N - 1 - i];
-		}
-		fillArray(0, new int[4]);
 		out.println(result);
 		out.close();
 	}
 
-	public static void fillArray(int count, int[] temp) {
-		if (count == 4) {
-			int minX = map[LEFT][temp[LEFT]];
-			int maxX = map[RIGHT][temp[RIGHT]];
-			int minY = map[TOP][temp[TOP]];
-			int maxY = map[BOTTOM][temp[BOTTOM]];
-			if (valid(minX, maxX, minY, maxY)) {
-				int area = area(minX, maxX, minY, maxY);
-				result = Math.min(result, area);
-			}
-			return;
-		}
-		for (int i = 0; i <= 3; i++) {
-			temp[count] = i;
-			fillArray(count + 1, temp);
-		}
-	}
-
-	public static int area(int minX, int maxX, int minY, int maxY) {
-		return (maxX - minX) * (maxY - minY);
-	}
-
-	public static boolean valid(int minX, int maxX, int minY, int maxY) {
-		if (maxX < minX || maxY < minY)
-			return false;
-		int total = 0;
+	public static boolean valid(int minX, int minY, int maxX, int maxY) {
+		int count = 0;
 		for (int i = 0; i < N; i++) {
 			if (cows[i][0] < minX || cows[i][0] > maxX || cows[i][1] < minY || cows[i][1] > maxY)
-				total++;
+				count++;
 		}
-		return total <= 3;
+		return count <= 1;
+	}
+
+	public static int area(int minX, int minY, int maxX, int maxY) {
+		return (maxX - minX) * (maxY - minY);
 	}
 }
