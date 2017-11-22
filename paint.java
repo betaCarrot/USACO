44664b01c6fda1179434a2e2c53aea789f02a271
betaCarrot <30773614+betaCarrot@users.noreply.github.com>
2017-11-21 23:00:12 -0500

    USACO December 2015 Cu #1

--- paint.java
+++ paint.java
@@ -1,5 +1,3 @@
-package solutions;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -9,35 +7,31 @@ import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
 public class paint {
+	private static int a, b, c, d;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("paint.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("paint.out")));
 		StringTokenizer st = new StringTokenizer(f.readLine());
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		StringTokenizer st1 = new StringTokenizer(f.readLine());
-		int c = Integer.parseInt(st1.nextToken());
-		int d = Integer.parseInt(st1.nextToken());
-		int result = solve(a, b, c, d);
-		out.println(result);
+		a = Integer.parseInt(st.nextToken());
+		b = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(f.readLine());
+		c = Integer.parseInt(st.nextToken());
+		d = Integer.parseInt(st.nextToken());
+		out.println(solve());
 		out.close();
 	}
 
-	public static int solve(int a, int b, int c, int d) {
-		if (a <= c) {
-			if (b <= c) {
-				return ((b - a) + (d - c));
-			} else if (b <= d) {
-				return d - a;
-			} else
-				return b - a;
-		} else {
-			if (d >= b) {
-				return d - c;
-			} else if (d <= a) {
-				return (b - a) + (d - c);
-			} else
-				return b - c;
+	public static int solve() {
+		if (d < a) {
+			return (d - c) + (b - a);
 		}
+		if (d <= b) {
+			return b - Math.min(a, c);
+		}
+		if (c > b) {
+			return (d - c) + (b - a);
+		}
+		return d - Math.min(a, c);
 	}
 }
