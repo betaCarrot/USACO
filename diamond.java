6febd4ccd8f19882961d05eaaf2e539df6575143
betaCarrot <30773614+betaCarrot@users.noreply.github.com>
2017-12-15 12:55:39 +0800

    USACO Open 2016 Ag #2

--- diamond.java
+++ diamond.java
@@ -4,13 +4,14 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class diamond {
-	private static int[] dp = new int[10001];
-	private static int[] counts = new int[10001];
+	private static int[] array;
+	private static int[] maxs;
+	private static int[] dp;
 	private static int N, K;
-	private static int result = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("diamond.in"));
@@ -18,19 +19,36 @@ public class diamond {
 		StringTokenizer st = new StringTokenizer(f.readLine());
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
+		array = new int[N];
+		maxs = new int[N];
+		dp = new int[N];
 		for (int i = 0; i < N; i++) {
-			counts[Integer.parseInt(f.readLine())]++;
+			array[i] = Integer.parseInt(f.readLine()) * 2;
 		}
-		for (int i = 1; i <= 10000; i++) {
-			dp[i] = dp[i - 1] + counts[i];
+		Arrays.sort(array);
+		for (int i = 0; i < N; i++) {
+			int next = array[i] + 2 * K + 1;
+			int nextIndex = -Arrays.binarySearch(array, next) - 1;
+			maxs[i] = nextIndex - i;
 		}
-		for (int i = 1; i <= 10000; i++) {
-			if (i + K > 10000)
+		for (int i = N - 2; i >= 0; i--) {
+			dp[i] = Math.max(dp[i + 1], maxs[i]);
+		}
+		int result = 0;
+		for (int i = 0; i < N; i++) {
+			int first = maxs[i];
+			int secondIndex = i + maxs[i];
+			if (secondIndex >= N)
 				break;
-			int amount = dp[i + K] - dp[i - 1];
-			result = Math.max(result, amount);
+			result = Math.max(result, first + dp[secondIndex]);
 		}
 		out.println(result);
 		out.close();
+	}
+
+	public static void printArray(int[] array) {
+		for (int i : array)
+			System.out.print(i + " ");
+		System.out.println();
 	}
 }
