754afd5ba2c904a92b7a7c8dc552d78d4c9e2ef1
betaCarrot <30773614+betaCarrot@users.noreply.github.com>
2018-01-04 22:03:17 +0800

    USACO 2.3.1

--- nocows.java
+++ nocows.java
@@ -1,33 +1,47 @@
+
 /*
  ID: majesti2
  LANG: JAVA
- PROG: nocows
+ TASK: nocows
  */
-import java.io.*;
-import java.util.*;
-import java.lang.Math;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
-public class nocows{
+public class nocows {
+	private static int N, K;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("nocows.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("nocows.out")));
 		StringTokenizer st = new StringTokenizer(f.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		long[][] dp = new long[N+1][K+1];
-		for(int i=1;i<=K;i++) dp[1][i] = 1;
-		for(int k=1;k<=K;k++){
-			for(int n=1;n<=N;n+=2){
-				for(int i=1;i<n-1;i++){
-					int j = n-1-i;
-					dp[n][k] += dp[i][k-1]*dp[j][k-1];
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		long[][] dp = new long[101][202];
+		long[][] smalltrees = new long[101][202];
+		dp[1][1] = 1;
+		for (int i = 2; i <= K; i++) {
+			for (int j = 1; j <= N; j += 2) {
+				for (int k = 1; k <= j - 1 - k; k += 2) {
+					int c = 1;
+					if (k != j - k - 1)
+						c = 2;
+					dp[i][j] += (dp[i - 1][k] * smalltrees[i - 2][j - k - 1]
+							+ smalltrees[i - 2][k] * dp[i - 1][j - k - 1] + dp[i - 1][k] * dp[i - 1][j - k - 1]) * c;
+					dp[i][j] %= 9901;
 				}
-				dp[n][k] = dp[n][k]%9901;
+			}
+			for (int k = 0; k <= N; k++) {
+				smalltrees[i - 1][k] = dp[i - 1][k] + smalltrees[i - 2][k];
+				smalltrees[i - 1][k] %= 9901;
 			}
 		}
-		if(dp[N][K]<dp[N][K-1]) dp[N][K] += 9901;
-		out.println((dp[N][K]-dp[N][K-1])%9901);
+		System.err.println(dp[K][N]);
+		out.println(dp[K][N] % 9901);
 		out.close();
 	}
 }
