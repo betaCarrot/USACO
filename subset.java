b73c5324cf637840a9826af5aa0375220be5a6fb
betaCarrot <30773614+betaCarrot@users.noreply.github.com>
2018-01-04 22:10:11 +0800

    USACO 2.2.2

--- subset.java
+++ subset.java
@@ -1,46 +1,39 @@
+
 /*
  ID: majesti2
  LANG: JAVA
  TASK: subset
  */
-
-import java.util.*;
-import java.io.*;
-import java.lang.Math;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class subset {
-	private static long[] possibleSums;
-	private static int target;
-	
-	public static void main (String [] args) throws IOException {
+	private static int N;
+	private static final int MAX = 781;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("subset.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("subset.out")));
-		StringTokenizer st = new StringTokenizer(f.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int sum = (1+N)*N/2;
-		possibleSums = new long[sum+1];
-		if(sum%2!=0){
-			out.println(0);
-		}
-		else{
-			target = sum/2;
-			possibleSums[0]++;
-			possibleSums[1]++;
-			for(int i=2;i<=N;i++){
-				int currentSize=0;
-				while(possibleSums[currentSize]!=0) currentSize++;
-				long[] temp = new long[sum+1];
-				for(int m=0;m<sum;m++) temp[m] = possibleSums[m];
-				for(int k=0;k<currentSize;k++){
-					possibleSums[k+i]+=temp[k];
+		N = Integer.parseInt(f.readLine());
+		long[][] dp = new long[N + 1][MAX];
+		dp[1][0] = dp[1][1] = 1L;
+		for (int i = 2; i <= N; i++) {
+			for (int j = 0; j < MAX; j++) {
+				dp[i][j] = dp[i - 1][j];
+				if (j >= i) {
+					dp[i][j] += dp[i - 1][j - i];
 				}
 			}
-			out.println(possibleSums[target]/2);
 		}
+		if (((N + 1) * N / 2) % 2 == 1)
+			out.println(0);
+		else
+			out.println(dp[N][(N + 1) * N / 4] / 2);
+		System.err.println(dp[N][(N + 1) * N / 4] / 2);
 		out.close();
-	}			
+	}
 }
-
-
-	
-	
