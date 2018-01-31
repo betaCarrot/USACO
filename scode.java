35e01b66a51d069f2cdcbd9fc99d298f35465ec7
betaCarrot <30773614+betaCarrot@users.noreply.github.com>
2018-01-31 16:37:58 -0500

    USACO February 2014 Ag #3

--- scode.java
+++ scode.java
@@ -6,39 +6,54 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class scode {
-	private static int result = 0;
+	private static int N;
+	private static String s;
+	private static final int MOD = 2014;
+	private static int[][] values;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("scode.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("scode.out")));
-		String line = f.readLine();
-		dfs(line);
-		out.println(result);
+		s = f.readLine();
+		N = s.length();
+		values = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int k = 0; k < N; k++) {
+				values[i][k] = -1;
+			}
+		}
+		out.println(dfs(0, N - 1));
 		out.close();
 	}
 
-	public static void dfs(String s) {
-		if (s.length() % 2 == 0 || s.length() == 1)
-			return;
-		String first = s.substring(0, s.length() / 2 + 1);
-		String second = s.substring(s.length() / 2 + 1);
-		if (first.substring(0, first.length() - 1).equals(second)) {
-			result++;
-			dfs(first);
+	public static int dfs(int li, int ri) {
+		if ((ri - li) <= 1) {
+			return 0;
 		}
-		if (first.substring(1).equals(second)) {
-			result++;
-			dfs(first);
+		if (values[li][ri] != -1) {
+			return values[li][ri];
 		}
-		String third = s.substring(0, s.length() / 2);
-		String fourth = s.substring(s.length() / 2);
-		if (fourth.substring(0, fourth.length() - 1).equals(third)) {
-			result++;
-			dfs(fourth);
+		int result = 0;
+		for (int i = 0; i < (ri - li) / 2; i++) {
+			String first = s.substring(li, li + i + 1);
+			String second = s.substring(li + i + 1, ri + 1);
+			String third = s.substring(ri - i, ri + 1);
+			String fourth = s.substring(li, ri - i);
+			if (second.startsWith(first)) {
+				result += 1 + dfs(li + i + 1, ri);
+			}
+			if (second.endsWith(first)) {
+				result += 1 + dfs(li + i + 1, ri);
+			}
+			if (fourth.startsWith(third)) {
+				result += 1 + dfs(li, ri - i - 1);
+			}
+			if (fourth.endsWith(third)) {
+				result += 1 + dfs(li, ri - i - 1);
+			}
 		}
-		if (fourth.substring(1).equals(third)) {
-			result++;
-			dfs(fourth);
-		}
+		result %= MOD;
+		values[li][ri] = result;
+		return result;
 	}
 }
