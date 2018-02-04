01b666c8dbd134e34bc9ab7b8963e6225669692e
betaCarrot <30773614+betaCarrot@users.noreply.github.com>
2018-02-03 21:10:23 -0500

    USACO December 2014 Ag #1

--- piggyback.java
+++ piggyback.java
@@ -4,16 +4,14 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class piggyback {
-	private static pathInc[] pathIncs;
-	private static pathDec[] pathDecs;
-	private static int[] distances0;
-	private static int[] distances1;
-	private static int[] distancesN;
 	private static int B, E, P, N, M;
+	private static ArrayList<ArrayList<Integer>> edges = new ArrayList<ArrayList<Integer>>();
+	private static int[] distances1, distances2, distancesN;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("piggyback.in"));
@@ -24,215 +22,102 @@ public class piggyback {
 		P = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		pathIncs = new pathInc[M];
-		pathDecs = new pathDec[M];
-		distances0 = new int[N];
 		distances1 = new int[N];
+		distances2 = new int[N];
 		distancesN = new int[N];
+		for (int i = 0; i < N; i++) {
+			edges.add(new ArrayList<Integer>());
+		}
+		for (int i = 0; i < N; i++) {
+			distances1[i] = -1;
+			distances2[i] = -1;
+			distancesN[i] = -1;
+		}
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(f.readLine());
-			int a = Integer.parseInt(st.nextToken()) - 1;
-			int b = Integer.parseInt(st.nextToken()) - 1;
-			pathIncs[i] = new pathInc(a, b);
-			pathDecs[i] = new pathDec(a, b);
+			int from = Integer.parseInt(st.nextToken()) - 1;
+			int to = Integer.parseInt(st.nextToken()) - 1;
+			edges.get(from).add(to);
+			edges.get(to).add(from);
 		}
-		Arrays.sort(pathIncs);
-		Arrays.sort(pathDecs);
-		shortestPath0();
-		shortestPath1();
-		shortestPathN();
+		bfs1();
+		bfs2();
+		bfsN();
 		int result = Integer.MAX_VALUE;
 		for (int i = 0; i < N; i++) {
-			result = Math.min(result, distances0[i] * B + distances1[i] * E + distancesN[i] * P);
+			result = Math.min(result, B * distances1[i] + E * distances2[i] + P * distancesN[i]);
 		}
 		out.println(result);
 		out.close();
 	}
 
-	public static void shortestPath0() {
-		boolean[] nextToVisit = new boolean[N];
-		boolean[] visiting = new boolean[N];
-		boolean[] visited = new boolean[N];
-		visiting[0] = true;
+	public static void bfs1() {
+		ArrayDeque<Integer> queue = new ArrayDeque<Integer>();
+		queue.offer(0);
 		int count = 0;
-		while (true) {
-			int numVisited = 0;
-			for (int i = 0; i < N; i++) {
-				if (visiting[i]) {
-					numVisited++;
-					visited[i] = true;
-					distances0[i] = count;
-					int incIndexLow = -Arrays.binarySearch(pathIncs, new pathInc(i, i)) - 1;
-					int incIndexHigh = -Arrays.binarySearch(pathIncs, new pathInc(i, Integer.MAX_VALUE)) - 1;
-					for (int index = incIndexLow; index < incIndexHigh; index++) {
-						int j = pathIncs[index].getB();
-						if (!visited[j]) {
-							nextToVisit[j] = true;
-						}
-					}
-					int decIndexLow = -Arrays.binarySearch(pathDecs, new pathDec(i, i)) - 1;
-					int decIndexHigh = -Arrays.binarySearch(pathDecs, new pathDec(i, Integer.MIN_VALUE)) - 1;
-					for (int index = decIndexLow; index < decIndexHigh; index++) {
-						int j = pathDecs[index].getB();
-						if (!visited[j]) {
-							nextToVisit[j] = true;
-						}
-					}
+		while (!queue.isEmpty()) {
+			ArrayDeque<Integer> temp = new ArrayDeque<Integer>();
+			while (!queue.isEmpty()) {
+				int i = queue.poll();
+				if (distances1[i] != -1) {
+					continue;
+				}
+				distances1[i] = count;
+				for (int j : edges.get(i)) {
+					temp.offer(j);
 				}
 			}
-			for (int i = 0; i < N; i++) {
-				visiting[i] = nextToVisit[i];
-			}
-			nextToVisit = new boolean[N];
+			queue.addAll(temp);
 			count++;
-			if (numVisited == 0)
-				break;
 		}
 	}
 
-	public static void shortestPathN() {
-		boolean[] nextToVisit = new boolean[N];
-		boolean[] visiting = new boolean[N];
-		boolean[] visited = new boolean[N];
-		visiting[N - 1] = true;
+	public static void bfs2() {
+		ArrayDeque<Integer> queue = new ArrayDeque<Integer>();
+		queue.offer(1);
 		int count = 0;
-		while (true) {
-			int numVisited = 0;
-			for (int i = 0; i < N; i++) {
-				if (visiting[i]) {
-					numVisited++;
-					visited[i] = true;
-					distancesN[i] = count;
-					int incIndexLow = -Arrays.binarySearch(pathIncs, new pathInc(i, i)) - 1;
-					int incIndexHigh = -Arrays.binarySearch(pathIncs, new pathInc(i, Integer.MAX_VALUE)) - 1;
-					for (int index = incIndexLow; index < incIndexHigh; index++) {
-						int j = pathIncs[index].getB();
-						if (!visited[j]) {
-							nextToVisit[j] = true;
-						}
-					}
-					int decIndexLow = -Arrays.binarySearch(pathDecs, new pathDec(i, i)) - 1;
-					int decIndexHigh = -Arrays.binarySearch(pathDecs, new pathDec(i, Integer.MIN_VALUE)) - 1;
-					for (int index = decIndexLow; index < decIndexHigh; index++) {
-						int j = pathDecs[index].getB();
-						if (!visited[j]) {
-							nextToVisit[j] = true;
-						}
-					}
+		while (!queue.isEmpty()) {
+			ArrayDeque<Integer> temp = new ArrayDeque<Integer>();
+			while (!queue.isEmpty()) {
+				int i = queue.poll();
+				if (distances2[i] != -1) {
+					continue;
+				}
+				distances2[i] = count;
+				for (int j : edges.get(i)) {
+					temp.offer(j);
 				}
 			}
-			for (int i = 0; i < N; i++) {
-				visiting[i] = nextToVisit[i];
-			}
-			nextToVisit = new boolean[N];
+			queue.addAll(temp);
 			count++;
-			if (numVisited == 0)
-				break;
 		}
 	}
 
-	public static void shortestPath1() {
-		boolean[] nextToVisit = new boolean[N];
-		boolean[] visiting = new boolean[N];
-		boolean[] visited = new boolean[N];
-		visiting[1] = true;
+	public static void bfsN() {
+		ArrayDeque<Integer> queue = new ArrayDeque<Integer>();
+		queue.offer(N - 1);
 		int count = 0;
-		while (true) {
-			int numVisited = 0;
-			for (int i = 0; i < N; i++) {
-				if (visiting[i]) {
-					numVisited++;
-					visited[i] = true;
-					distances1[i] = count;
-					int incIndexLow = -Arrays.binarySearch(pathIncs, new pathInc(i, i)) - 1;
-					int incIndexHigh = -Arrays.binarySearch(pathIncs, new pathInc(i, Integer.MAX_VALUE)) - 1;
-					for (int index = incIndexLow; index < incIndexHigh; index++) {
-						int j = pathIncs[index].getB();
-						if (!visited[j]) {
-							nextToVisit[j] = true;
-						}
-					}
-					int decIndexLow = -Arrays.binarySearch(pathDecs, new pathDec(i, i)) - 1;
-					int decIndexHigh = -Arrays.binarySearch(pathDecs, new pathDec(i, Integer.MIN_VALUE)) - 1;
-					for (int index = decIndexLow; index < decIndexHigh; index++) {
-						int j = pathDecs[index].getB();
-						if (!visited[j]) {
-							nextToVisit[j] = true;
-						}
-					}
+		while (!queue.isEmpty()) {
+			ArrayDeque<Integer> temp = new ArrayDeque<Integer>();
+			while (!queue.isEmpty()) {
+				int i = queue.poll();
+				if (distancesN[i] != -1) {
+					continue;
+				}
+				distancesN[i] = count;
+				for (int j : edges.get(i)) {
+					temp.offer(j);
 				}
 			}
-			for (int i = 0; i < N; i++) {
-				visiting[i] = nextToVisit[i];
-			}
-			nextToVisit = new boolean[N];
+			queue.addAll(temp);
 			count++;
-			if (numVisited == 0)
-				break;
 		}
 	}
 
 	public static void printArray(int[] array) {
-		for (int i : array)
+		for (int i : array) {
 			System.out.print(i + " ");
+		}
 		System.out.println();
-	}
-}
-
-class pathInc implements Comparable<pathInc> {
-	private int a;
-	private int b;
-
-	public pathInc(int a, int b) {
-		this.a = Math.min(a, b);
-		this.b = Math.max(a, b);
-	}
-
-	public int getA() {
-		return a;
-	}
-
-	public int getB() {
-		return b;
-	}
-
-	public String toString() {
-		return a + " " + b;
-	}
-
-	public int compareTo(pathInc next) {
-		if (Integer.compare(a, next.a) != 0) {
-			return Integer.compare(a, next.a);
-		}
-		return Integer.compare(b, next.b);
-	}
-}
-
-class pathDec implements Comparable<pathDec> {
-	private int a;
-	private int b;
-
-	public pathDec(int a, int b) {
-		this.a = Math.max(a, b);
-		this.b = Math.min(a, b);
-	}
-
-	public int getA() {
-		return a;
-	}
-
-	public int getB() {
-		return b;
-	}
-
-	public String toString() {
-		return a + " " + b;
-	}
-
-	public int compareTo(pathDec next) {
-		if (Integer.compare(a, next.a) != 0) {
-			return -Integer.compare(a, next.a);
-		}
-		return -Integer.compare(b, next.b);
 	}
 }
