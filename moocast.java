d9a0e3f487c16b30fc900883318f9fb97782a19f
betaCarrot <30773614+betaCarrot@users.noreply.github.com>
2018-02-09 21:04:15 -0500

    USACO December 2016 Au #1

--- moocast.java
+++ moocast.java
@@ -7,56 +7,67 @@ import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
 public class moocast {
-	private static int[] powers;
-	private static int[][] map;
-	private static boolean[] visited;
 	private static int N;
-	private static int count = 0;
-	private static int result = 0;
+	private static int[][] cows;
+	private static boolean[] visited;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader f = new BufferedReader(new FileReader("moocast.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("moocast.out")));
 		N = Integer.parseInt(f.readLine());
-		powers = new int[N];
-		map = new int[N][2];
+		cows = new int[N][2];
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(f.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			int p = Integer.parseInt(st.nextToken());
-			map[i][0] = x;
-			map[i][1] = y;
-			powers[i] = p;
+			cows[i][0] = x;
+			cows[i][1] = y;
 		}
-		visited = new boolean[N];
-		for (int i = 0; i < N; i++) {
-			dfs(i);
-			result = Math.max(result, count);
-			count = 0;
-			visited = new boolean[N];
-		}
+		int result = binarySearch();
 		out.println(result);
 		out.close();
 	}
 
-	public static void dfs(int index) {
-		count++;
+	public static int binarySearch() {
+		int low = 0;
+		int high = 625000001;
+		while (low != high) {
+			int mid = (low + high) / 2;
+			if (valid(mid)) {
+				high = mid;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return (high + low) / 2;
+	}
+
+	public static boolean valid(int K) {
+		visited = new boolean[N];
+		visit(0, K);
+		for (boolean b : visited) {
+			if (!b)
+				return false;
+		}
+		return true;
+	}
+
+	public static void visit(int index, int K) {
+		if (visited[index]) {
+			return;
+		}
 		visited[index] = true;
-		int x1 = map[index][0];
-		int y1 = map[index][1];
 		for (int i = 0; i < N; i++) {
 			if (visited[i])
 				continue;
-			int x2 = map[i][0];
-			int y2 = map[i][1];
-			if (distance(y1, x1, y2, x2) < powers[index]) {
-				dfs(i);
+			if (distance(i, index) <= K) {
+				visit(i, K);
 			}
 		}
 	}
 
-	public static double distance(int y1, int x1, int y2, int x2) {
-		return Math.sqrt((y1 - y2) * (y1 - y2) + (x1 - x2) * (x1 - x2));
+	public static int distance(int i, int j) {
+		return (cows[i][0] - cows[j][0]) * (cows[i][0] - cows[j][0])
+				+ (cows[i][1] - cows[j][1]) * (cows[i][1] - cows[j][1]);
 	}
 }
