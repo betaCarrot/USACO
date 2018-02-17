a611f96d4ffe535678b10134ce47f62dc295bff4
betaCarrot <30773614+betaCarrot@users.noreply.github.com>
2018-02-17 00:24:28 -0500

    USACO December 2011 Ag #1

--- photo.java
+++ photo.java
@@ -4,57 +4,60 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 
 public class photo {
 	private static int N;
+	private static ArrayList<Integer> list = new ArrayList<Integer>();
+	private static int[][] pos;
+	private static HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+	private static Comparator<Integer> comp = new Comparator<Integer>() {
+		public int compare(Integer a, Integer b) {
+			int indexA = map.get(a);
+			int indexB = map.get(b);
+			int count = 0;
+			for (int i = 0; i < 5; i++) {
+				if (pos[i][indexA] < pos[i][indexB]) {
+					count++;
+				}
+			}
+			if (count >= 3)
+				return -1;
+			else
+				return 1;
+		}
+	};
 
 	public static void main(String[] args) throws IOException {
+		long startTime = System.currentTimeMillis();
 		BufferedReader f = new BufferedReader(new FileReader("photo.in"));
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("photo.out")));
-		StringTokenizer st = new StringTokenizer(f.readLine());
-		N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		PriorityQueue<pair> pairs = new PriorityQueue<pair>();
-		for (int i = 0; i < K; i++) {
-			st = new StringTokenizer(f.readLine());
-			pairs.offer(new pair(Integer.parseInt((st.nextToken())), Integer.parseInt((st.nextToken()))));
+		PrintWriter out = new PrintWriter(
+				new BufferedWriter(new FileWriter("C:\\Users\\majesticdennis\\Desktop\\judge\\photo.out")));
+		N = Integer.parseInt(f.readLine());
+		pos = new int[5][N];
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(f.readLine().trim());
+			list.add(num);
+			map.put(num, i);
+			pos[0][i] = i;
 		}
-		int count = 1;
-		int lastFirst = 0;
-		while (!pairs.isEmpty()) {
-			int minSecond = Integer.MAX_VALUE;
-			while (!pairs.isEmpty() && pairs.peek().getFirst() < minSecond) {
-				pair p = pairs.poll();
-				lastFirst = Math.max(lastFirst, p.getFirst());
-				minSecond = Math.min(minSecond, p.getSecond());
+		for (int c = 1; c <= 4; c++) {
+			for (int i = 0; i < N; i++) {
+				int num = Integer.parseInt(f.readLine());
+				int index = map.get(num);
+				pos[c][index] = i;
 			}
-			count++;
 		}
-		out.println(count);
+		Collections.sort(list, comp);
+		StringBuilder sb = new StringBuilder();
+		for (int i : list) {
+			sb.append(i).append("\n");
+		}
+		out.print(sb.toString());
 		out.close();
-	}
-}
-
-class pair implements Comparable<pair> {
-	int first;
-	int second;
-
-	public pair(int a, int b) {
-		first = Math.min(a, b);
-		second = Math.max(a, b);
-	}
-
-	public int getFirst() {
-		return first;
-	}
-
-	public int getSecond() {
-		return second;
-	}
-
-	public int compareTo(pair next) {
-		return Integer.compare(first, next.first);
+		System.err.println(System.currentTimeMillis() - startTime);
 	}
 }
