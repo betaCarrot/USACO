3f37888ac885c274656c220de42cdb7293e9ffe8
betaCarrot <30773614+betaCarrot@users.noreply.github.com>
2018-02-17 00:24:09 -0500

    USACO February 2014 Ag #2 dijkstra+pq+parent

--- rblock.java
+++ rblock.java
@@ -9,7 +9,7 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class rblock {
-	private static int N, M, initDistance;
+	private static int N, M;
 	private static int[] parent;
 	private static ArrayList<ArrayList<edge>> edges = new ArrayList<ArrayList<edge>>();
 
@@ -21,54 +21,38 @@ public class rblock {
 		M = Integer.parseInt(st.nextToken());
 		parent = new int[N];
 		for (int i = 0; i < N; i++) {
-			parent[i] = -1;
-		}
-		for (int i = 0; i < N; i++) {
 			edges.add(new ArrayList<edge>());
 		}
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(f.readLine());
 			int from = Integer.parseInt(st.nextToken()) - 1;
 			int to = Integer.parseInt(st.nextToken()) - 1;
-			int distance = Integer.parseInt(st.nextToken());
-			edges.get(from).add(new edge(to, distance));
-			edges.get(to).add(new edge(from, distance));
+			int weight = Integer.parseInt(st.nextToken());
+			edges.get(from).add(new edge(to, weight));
+			edges.get(to).add(new edge(from, weight));
 		}
-		init();
-		int index = N - 1;
+		int initDistance = init();
 		int result = 0;
-		while (index != 0) {
-			int firstIndex = -1;
-			int secondIndex = -1;
-			for (int i = 0; i < edges.get(index).size(); i++) {
-				if (edges.get(index).get(i).to == parent[index]) {
-					edges.get(index).get(i).distance *= 2;
-					firstIndex = i;
-					break;
-				}
+		for (int i = 0; i < N; i++) {
+			for (edge e : edges.get(i)) {
+				if (parent[e.to] != i)
+					continue;
+				e.weight *= 2;
+				result = Math.max(result, solve() - initDistance);
+				e.weight /= 2;
 			}
-			for (int i = 0; i < edges.get(parent[index]).size(); i++) {
-				if (edges.get(parent[index]).get(i).to == index) {
-					edges.get(parent[index]).get(i).distance *= 2;
-					secondIndex = i;
-					break;
-				}
-			}
-			result = Math.max(result, solve() - initDistance);
-			edges.get(index).get(firstIndex).distance /= 2;
-			edges.get(parent[index]).get(secondIndex).distance /= 2;
-			index = parent[index];
 		}
 		out.println(result);
 		out.close();
 	}
 
 	public static int solve() {
+		PriorityQueue<node> pq = new PriorityQueue<node>();
 		int[] distances = new int[N];
-		for (int i = 1; i < N; i++) {
+		for (int i = 0; i < N; i++) {
 			distances[i] = Integer.MAX_VALUE;
 		}
-		PriorityQueue<node> pq = new PriorityQueue<node>();
+		distances[0] = 0;
 		pq.offer(new node(0, 0));
 		while (!pq.isEmpty()) {
 			node curr = pq.poll();
@@ -76,7 +60,7 @@ public class rblock {
 			int distance = curr.distance;
 			for (edge e : edges.get(i)) {
 				int j = e.to;
-				int weight = e.distance;
+				int weight = e.weight;
 				if (distance + weight < distances[j]) {
 					distances[j] = distance + weight;
 					pq.offer(new node(j, distances[j]));
@@ -86,12 +70,13 @@ public class rblock {
 		return distances[N - 1];
 	}
 
-	public static void init() {
+	public static int init() {
+		PriorityQueue<node> pq = new PriorityQueue<node>();
 		int[] distances = new int[N];
-		for (int i = 1; i < N; i++) {
+		for (int i = 0; i < N; i++) {
 			distances[i] = Integer.MAX_VALUE;
 		}
-		PriorityQueue<node> pq = new PriorityQueue<node>();
+		distances[0] = 0;
 		pq.offer(new node(0, 0));
 		while (!pq.isEmpty()) {
 			node curr = pq.poll();
@@ -99,7 +84,7 @@ public class rblock {
 			int distance = curr.distance;
 			for (edge e : edges.get(i)) {
 				int j = e.to;
-				int weight = e.distance;
+				int weight = e.weight;
 				if (distance + weight < distances[j]) {
 					distances[j] = distance + weight;
 					pq.offer(new node(j, distances[j]));
@@ -107,16 +92,7 @@ public class rblock {
 				}
 			}
 		}
-		initDistance = distances[N - 1];
-	}
-}
-
-class edge {
-	public int to, distance;
-
-	public edge(int b, int c) {
-		to = b;
-		distance = c;
+		return distances[N - 1];
 	}
 }
 
@@ -130,5 +106,14 @@ class node implements Comparable<node> {
 
 	public int compareTo(node next) {
 		return distance - next.distance;
+	}
+}
+
+class edge {
+	public int to, weight;
+
+	public edge(int a, int b) {
+		to = a;
+		weight = b;
 	}
 }
